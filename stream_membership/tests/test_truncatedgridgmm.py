d7236b8d6e5e0a76f2e0d6fab1efd925873dc3de
Adrian Price-Whelan <adrian.prw@gmail.com>
2023-06-21 07:15:23 -0400

    add numpyro distribution for grid GMM and simple test

---
+++ stream_membership/tests/test_truncatedgridgmm.py
@@ -0,0 +1,89 @@
+import warnings
+
+import jax.numpy as jnp
+import numpy as np
+import numpyro.distributions as dist
+import pytest
+
+from ..truncatedgridgmm import TruncatedGridGMM
+
+
+@pytest.mark.parametrize(
+    "kwargs",
+    [
+        dict(  # Two 1D distributions, truncated low/high:
+            probs=[1.0, 0.2],
+            locs=[[1.0], [2.0]],
+            scales=[[0.25], [0.1]],
+            low=0.5,
+            high=2.1,
+        ),
+        dict(  # Three 2D distributions, truncated low:
+            probs=[1.0, 0.5, 0.2],
+            locs=[[1.0, 2.0], [1.5, 0.5], [0.5, 1.0]],
+            scales=0.2,
+            low=[0.2, 0.0],
+        ),
+        dict(  # Three 2D distributions, truncated low but only one dim:
+            probs=[1.0, 0.5, 0.2],
+            locs=[[1.0, 2.0], [1.5, 0.5], [0.5, 1.0]],
+            scales=0.2,
+            low=[0.2, -np.inf],
+        ),
+        dict(  # Two 3D distributions, truncated high:
+            probs=[1.0, 0.2],
+            locs=[[1.0, 2.0, 0.0], [1.5, 0.5, -1]],
+            scales=[[1.0, 1.0, 2.0], [2, 1, 1]],
+            high=[2.2, 1.5, 3.5],
+        ),
+    ],
+)
+def test_gridgmm(kwargs):
+    """
+    A mixture of two 1D distributions
+    """
+    mix = dist.Categorical(probs=jnp.array(kwargs.pop("probs")))
+    gmm = TruncatedGridGMM(mix, **kwargs)
+    gmm_notrunc = TruncatedGridGMM(
+        mix, **{k: v for k, v in kwargs.items() if k not in ["low", "high"]}
+    )
+    D = gmm._D
+
+    # low and high could be inf, so to generate random numbers we need to be safe:
+    val_high = np.broadcast_to(kwargs.get("high", np.inf), (D,))
+    val_low = np.broadcast_to(kwargs.get("low", -np.inf), (D,))
+    BIGNUM = int(1e6)
+    N_samples = 10
+
+    rng = np.random.default_rng(seed=42)
+    good_vals = rng.uniform(
+        np.max([val_low, np.full(D, -1e4)], axis=0),
+        np.min([val_high, np.full(D, 1e4)], axis=0),
+        (N_samples, D),
+    )
+    logprob_vals = gmm.log_prob(good_vals)
+    assert np.all(np.isfinite(logprob_vals))
+    assert np.all(logprob_vals >= gmm_notrunc.log_prob(good_vals))
+
+    # outside of truncated region
+    # only have 1 if val_low isn't -inf, same for 2
+    args = []
+    if np.any(np.isfinite(val_low)):
+        tmp = val_low.copy()
+        tmp[~np.isfinite(val_low)] = -0.5 * BIGNUM  # ???
+        args.append(rng.uniform(-BIGNUM, tmp, (N_samples, D)))
+
+    if np.any(np.isfinite(val_high)):
+        tmp = val_high.copy()
+        tmp[~np.isfinite(val_high)] = 0.5 * BIGNUM  # ???
+        args.append(rng.uniform(tmp, BIGNUM, (N_samples, D)))
+
+    bad_vals = np.concatenate(args)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        logprob_vals = gmm.log_prob(bad_vals)
+    assert np.all(~np.isfinite(logprob_vals))
+
+    # TODO: remove this after implementing sample()
+    with pytest.raises(NotImplementedError):
+        gmm.sample()
---
+++ stream_membership/truncatedgridgmm.py
@@ -0,0 +1,136 @@
+import jax
+import jax.numpy as jnp
+import numpyro.distributions as dist
+from jax import lax
+from numpyro.distributions import constraints
+from numpyro.distributions.constraints import _IndependentConstraint, _Interval
+from numpyro.distributions.util import promote_shapes
+
+__all__ = ["TruncatedGridGMM"]
+
+
+class _LeftExtendedReal(constraints.Constraint):
+    """
+    Any number in the interval [-inf, inf).
+    """
+
+    def __call__(self, x):
+        return (x == x) & (x != float("inf"))
+
+    def feasible_like(self, prototype):
+        return jnp.zeros_like(prototype)
+
+
+class _RightExtendedReal(constraints.Constraint):
+    """
+    Any number in the interval (-inf, inf].
+    """
+
+    def __call__(self, x):
+        return (x == x) & (x != float("-inf"))
+
+    def feasible_like(self, prototype):
+        return jnp.zeros_like(prototype)
+
+
+left_extended_real = _LeftExtendedReal()
+right_extended_real = _RightExtendedReal()
+
+
+class _IntervalVector(_IndependentConstraint):
+    def __init__(self, lower_bound, upper_bound):
+        super().__init__(_Interval(lower_bound, upper_bound), 1)
+
+
+interval_vector = _IntervalVector
+
+
+class TruncatedGridGMM(dist.mixtures.MixtureSameFamily):
+    """
+    A Gaussian Mixture Model where the components are fixed to their input locations and
+    there are no covariances (but each dimension can have different scales / standard
+    deviations). The model can also be truncated to a rectangular region.
+    """
+
+    arg_constraints = {
+        "locs": constraints.real,
+        "scales": constraints.positive,
+        "low": left_extended_real,
+        "high": right_extended_real,
+    }
+    reparametrized_params = ["locs", "scales", "low", "high"]
+
+    def __init__(
+        self,
+        mixing_distribution,
+        locs=0.0,
+        scales=1.0,
+        low=-float("inf"),
+        high=float("inf"),
+        *,
+        validate_args=True,
+    ):
+        dist.mixtures._check_mixing_distribution(mixing_distribution)
+
+        batch_shape = lax.broadcast_shapes(jnp.shape(locs), jnp.shape(scales))
+        if validate_args:
+            if len(batch_shape) != 2:
+                raise ValueError("TODO: Bork")
+
+        self.locs, self.scales = promote_shapes(
+            jnp.array(locs),
+            jnp.array(scales),
+            shape=batch_shape,
+        )
+        self._K, self._D = batch_shape
+
+        locs = jnp.broadcast_to(self.locs, batch_shape)
+        scales = jnp.broadcast_to(self.scales, batch_shape)
+
+        self.low = jnp.array(low)
+        self.high = jnp.array(high)
+        low = jnp.broadcast_to(self.low, batch_shape[1:])
+        high = jnp.broadcast_to(self.high, batch_shape[1:])
+
+        covs = jnp.zeros(batch_shape + (batch_shape[1],))
+        idx = jnp.arange(batch_shape[1])
+        covs = covs.at[:, idx, idx].set(scales**2)
+        component_distribution = dist.MultivariateNormal(
+            locs, covariance_matrix=covs, validate_args=validate_args
+        )
+
+        super().__init__(
+            mixing_distribution, component_distribution, validate_args=validate_args
+        )
+
+        self._support = interval_vector(low, high)
+
+        # Pre-compute the amount of probability mass that is inside the censored region
+        self._log_diff_tail_probs = jnp.zeros(batch_shape[0])
+        for k in range(batch_shape[0]):  # K Gaussian components
+            for d in range(batch_shape[1]):  # D dimensions
+                norm = dist.Normal(locs[k, d], scales[k, d])
+                sign = jnp.where(locs[k, d] >= low[d], 1.0, -1.0)
+
+                _tail_prob_at_low = norm.cdf(locs[k, d] - sign * (locs[k, d] - low[d]))
+                _tail_prob_at_high = norm.cdf(
+                    locs[k, d] - sign * (locs[k, d] - high[d])
+                )
+
+                self._log_diff_tail_probs = self._log_diff_tail_probs.at[k].add(
+                    jnp.log(sign * (_tail_prob_at_high - _tail_prob_at_low))
+                )
+
+    @constraints.dependent_property
+    def support(self):
+        return self._support
+
+    def component_log_probs(self, value):
+        value = jnp.expand_dims(value, self.mixture_dim)
+        component_log_probs = (
+            self.component_distribution.log_prob(value) - self._log_diff_tail_probs
+        )
+        return jax.nn.log_softmax(self.mixing_distribution.logits) + component_log_probs
+
+    def sample(self, *args, **kwargs):
+        raise NotImplementedError("sample() is not implemented yet")
