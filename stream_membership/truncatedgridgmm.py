e25bb3f8f9584f8c1a7737e960a999e43a88ed98
Adrian Price-Whelan <adrian.prw@gmail.com>
2023-06-27 18:05:10 -0400

    properly handle input shapes in truncatedgridgmm

--- stream_membership/truncatedgridgmm.py
+++ stream_membership/truncatedgridgmm.py
@@ -77,29 +77,42 @@ class TruncatedGridGMM(dist.mixtures.MixtureSameFamily):
         if high is None:
             high = float("inf")
 
-        batch_shape = lax.broadcast_shapes(jnp.shape(locs), jnp.shape(scales))
-        if validate_args:
-            if len(batch_shape) != 2:
-                raise ValueError("TODO: Bork")
+        # N = number of data points, K = mixture components, D = dimensions
+        # - event_shape is the dimensionality of the data - number of dependent
+        #   coordinates, i.e., "D" in the below
+        # - batch_shape is the number of independent dimensions. So, here, I think
+        #   should at minimum be "K", but when passing in different scales for each data
+        #   point to deconvolve, batch_shape should be (N, K). So we can assume that
+        #   the number of mixture components is batch_shape[-1].
+        combined_shape = lax.broadcast_shapes(jnp.shape(locs), jnp.shape(scales))
+        batch_shape = combined_shape[:-1]
+        event_shape = combined_shape[-1:]
+        K = batch_shape[-1]
+        D = event_shape[0]
+
+        # TODO: If we want to be strict: error if event_shape has len > 1
+
+        # TODO: check that mixing_distribution has the right shape
 
         self.locs, self.scales = promote_shapes(
             jnp.array(locs),
             jnp.array(scales),
-            shape=batch_shape,
+            shape=combined_shape,
         )
-        self._K, self._D = batch_shape
+        locs = jnp.broadcast_to(self.locs, combined_shape)
+        scales = jnp.broadcast_to(self.scales, combined_shape)
 
-        locs = jnp.broadcast_to(self.locs, batch_shape)
-        scales = jnp.broadcast_to(self.scales, batch_shape)
+        # TODO: low and high should be the same shape as event_shape
+        self.low, self.high = promote_shapes(
+            jnp.array(low), jnp.array(high), shape=event_shape
+        )
+        low = jnp.broadcast_to(self.low, event_shape)
+        high = jnp.broadcast_to(self.high, event_shape)
 
-        self.low = jnp.array(low)
-        self.high = jnp.array(high)
-        low = jnp.broadcast_to(self.low, batch_shape[1:])
-        high = jnp.broadcast_to(self.high, batch_shape[1:])
-
-        covs = jnp.zeros(batch_shape + (batch_shape[1],))
-        idx = jnp.arange(batch_shape[1])
-        covs = covs.at[:, idx, idx].set(scales**2)
+        # covs = jnp.zeros(batch_shape + event_shape + event_shape)
+        covs = jnp.zeros(self.scales.shape[:-1] + event_shape + event_shape)
+        idx = jnp.arange(D)
+        covs = covs.at[..., idx, idx].set(scales**2)
         component_distribution = dist.MultivariateNormal(
             locs, covariance_matrix=covs, validate_args=validate_args
         )
@@ -111,25 +124,27 @@ class TruncatedGridGMM(dist.mixtures.MixtureSameFamily):
         self._support = interval_vector(low, high)
 
         # Pre-compute the amount of probability mass that is inside the censored region
-        self._log_diff_tail_probs = jnp.zeros(batch_shape[0])
-        for k in range(batch_shape[0]):  # K Gaussian components
-            for d in range(batch_shape[1]):  # D dimensions
-                norm = dist.Normal(locs[k, d], scales[k, d])
-                sign = jnp.where(locs[k, d] >= low[d], 1.0, -1.0)
+        # We need to compute this for all batch_shape elements.
+        # TODO: some residual feeling of unease about this...
+        self._log_diff_tail_probs = jnp.zeros(batch_shape)
+        for k in range(K):  # K Gaussian components
+            for d in range(D):  # D dimensions
+                norm = dist.Normal(locs[..., k, d], scales[..., k, d])
+                sign = jnp.where(locs[..., k, d] >= low[d], 1.0, -1.0)
 
                 _tail_prob_at_low = jnp.where(
                     jnp.isfinite(low[d]),
-                    norm.cdf(locs[k, d] - sign * (locs[k, d] - low[d])),
+                    norm.cdf(locs[..., k, d] - sign * (locs[..., k, d] - low[d])),
                     0.0,
                 )
 
                 _tail_prob_at_high = jnp.where(
                     jnp.isfinite(high[d]),
-                    norm.cdf(locs[k, d] - sign * (locs[k, d] - high[d])),
+                    norm.cdf(locs[..., k, d] - sign * (locs[..., k, d] - high[d])),
                     1.0,
                 )
 
-                self._log_diff_tail_probs = self._log_diff_tail_probs.at[k].add(
+                self._log_diff_tail_probs = self._log_diff_tail_probs.at[..., k].add(
                     jnp.log(sign * (_tail_prob_at_high - _tail_prob_at_low))
                 )
 
