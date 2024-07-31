2d6a68c46d44a2abf0c4d274bc8fde1d5c9c96bc
Adrian Price-Whelan <adrian.prw@gmail.com>
2024-07-31 08:39:19 -0400

    implement sample and log_prob for dirichlet spline and add unit tests

--- stream_membership/numpyro_dist.py
+++ stream_membership/numpyro_dist.py
@@ -71,10 +71,34 @@ class NormalSpline(dist.Distribution):
     def sample(
         self, key: Any, sample_shape: Any = (), x: ArrayLike | None = None
     ) -> jax.Array | Any:
+        """
+        Draws samples from the distribution.
+
+        Parameters
+        ----------
+        key
+            JAX random number generator key.
+        sample_shape
+            Shape of the sample.
+        x
+            Array of x values at which to evaluate the splines. If not provided, the
+            x values provided at initialization will be used.
+        """
         helper = self._make_helper_dist(x)
         return helper.sample(key=key, sample_shape=sample_shape)
 
     def log_prob(self, value: ArrayLike, x: ArrayLike | None = None) -> jax.Array | Any:
+        """
+        Evaluates the log probability density for a batch of samples given by value.
+
+        Parameters
+        ----------
+        value
+            Array of samples to evaluate the log probability for.
+        x
+            Array of x values at which to evaluate the splines. If not provided, the
+            x values provided at initialization will be used.
+        """
         helper = self._make_helper_dist(x)
         return helper.log_prob(value)
 
@@ -171,22 +195,68 @@ class DirichletSpline(dist.Dirichlet):
         # Can have any shape
         self.x = jnp.array(x)
 
+        self.spline_k = int(spline_k)
+
         # Should have shape (n_components, n_knots)
         self.concentration_vals = jnp.array(concentration_vals)
         if jnp.ndim(concentration_vals) != 2:
             msg = "`concentration_vals` must be two dimensional."
             raise ValueError(msg)
 
-        spl = jax.vmap(eval_spline, in_axes=(None, 0, None, None))
-        conc = spl(self.knots, self.concentration_vals, self.x, spline_k)
+        self._spl = jax.vmap(eval_spline, in_axes=(None, 0, None, None))
+        conc = self._spl(self.knots, self.concentration_vals, self.x, self.spline_k)
 
+        super().__init__(
+            concentration=self._clean_conc(conc),
+            validate_args=validate_args,
+        )
+
+    @staticmethod
+    def _clean_conc(conc):
         # TODO: need to make sure conc values are > 0 -- either do this by passing
         # values through a transform, like sigmoid? Or like here, where we just
         # threshold:
         fl = jnp.float64 if jax.config.jax_enable_x64 else jnp.float32
-        conc = jnp.where(conc > 0, conc, jnp.finfo(fl).tiny).T
+        return jnp.where(conc > 0, conc, jnp.finfo(fl).tiny).T
 
-        super().__init__(
-            concentration=conc,
-            validate_args=validate_args,
-        )
+    def sample(
+        self, key: Any, sample_shape: Any = (), x: ArrayLike | None = None
+    ) -> jax.Array | Any:
+        """
+        Draws samples from the distribution.
+
+        Parameters
+        ----------
+        key
+            JAX random number generator key.
+        sample_shape
+            Shape of the sample.
+        x
+            Array of x values at which to evaluate the splines. If not provided, the
+            x values provided at initialization will be used.
+        """
+        if x is None:
+            return super().sample(key, sample_shape)
+
+        conc = self._spl(self.knots, self.concentration_vals, x, self.spline_k)
+        obj = dist.Dirichlet(concentration=self._clean_conc(conc))
+        return obj.sample(key, sample_shape)
+
+    def log_prob(self, value: ArrayLike, x: ArrayLike | None = None) -> jax.Array | Any:
+        """
+        Evaluates the log probability density for a batch of samples given by value.
+
+        Parameters
+        ----------
+        value
+            Array of samples to evaluate the log probability for.
+        x
+            Array of x values at which to evaluate the splines. If not provided, the
+            x values provided at initialization will be used.
+        """
+        if x is None:
+            return super().log_prob(value)
+
+        conc = self._spl(self.knots, self.concentration_vals, x, self.spline_k)
+        obj = dist.Dirichlet(concentration=self._clean_conc(conc))
+        return obj.log_prob(value)
---
+++ stream_membership/tests/test_numpyro_dist.py
@@ -0,0 +1,69 @@
+import jax
+import jax.numpy as jnp
+import numpy as np
+import pytest
+
+from ..numpyro_dist import DirichletSpline, NormalSpline, TruncatedNormalSpline
+
+
+def make_dists(seed=867):
+    rng = np.random.default_rng(seed)
+
+    knots = jnp.linspace(0, 10, 8)
+    x = jnp.linspace(1, 8, 32)
+
+    dists = []
+
+    d = NormalSpline(
+        knots=knots,
+        loc_vals=rng.uniform(-5, 5, knots.size),
+        ln_scale_vals=rng.uniform(-1, 1, knots.size),
+        x=x,
+    )
+    dists.append(d)
+
+    d = TruncatedNormalSpline(
+        knots=knots,
+        loc_vals=rng.uniform(-5, 5, knots.size),
+        ln_scale_vals=rng.uniform(-1, 1, knots.size),
+        low=-4.0,
+        high=4.0,
+        x=x,
+    )
+    dists.append(d)
+
+    d = DirichletSpline(
+        knots=knots, concentration_vals=rng.uniform(0, 1, (3, knots.size)), x=x
+    )
+    dists.append(d)
+
+    return dists
+
+
+@pytest.mark.parametrize("dist_", make_dists())
+def test_methods(dist_):
+    # New x grid to be used below:
+    x2 = jnp.linspace(1, 8, 64)
+
+    s = dist_.sample(jax.random.PRNGKey(0))
+    assert s.shape == (dist_.x.size, *dist_.event_shape)
+    s = dist_.sample(jax.random.PRNGKey(1), sample_shape=(10,))
+    assert s.shape == (10, dist_.x.size, *dist_.event_shape)
+
+    # Test sample with a new x grid:
+    s = dist_.sample(jax.random.PRNGKey(2), x=x2)
+    assert s.shape == (x2.size, *dist_.event_shape)
+    s = dist_.sample(jax.random.PRNGKey(3), x=x2, sample_shape=(10,))
+    assert s.shape == (10, x2.size, *dist_.event_shape)
+
+    # Test evaluating log_prob with previously provided x grid:
+    shape = dist_.x.shape + dist_.event_shape
+    y = np.linspace(0, 1, np.prod(shape)).reshape(shape)
+    lp = dist_.log_prob(y)
+    assert lp.shape == dist_.x.shape
+
+    # Test evaluating log_prob with a different x grid:
+    shape = x2.shape + dist_.event_shape
+    y2 = np.linspace(0, 1, np.prod(shape)).reshape(shape)
+    lp = dist_.log_prob(y2, x=x2)
+    assert lp.shape == x2.shape
