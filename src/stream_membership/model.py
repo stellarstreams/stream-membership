fixing scaling implementation in plot_model_projections and taking out gpu stuff

src/stream_membership/model.py
@@ -65,7 +65,7 @@ class ModelMixin:
         self,
         pars: dict[str, Any],
         grids: dict[str, ArrayLike],
-        data: dict[str, Any] = None, #for scaling to data when making data+model+residual plots
+        ndata: dict[str, Any] = None, #for scaling to data when making data+model+residual plots
         grid_coord_names: list[tuple[str, str]] | None = None,
         x_coord_name: str | None = None,
         axes: mpl_axes.Axes | None = None,
@@ -111,12 +111,10 @@ class ModelMixin:
             x_coord_name=x_coord_name,
         )
 
-        if data == None:
+        if ndata == None:
             ims = {k: np.exp(v) for k, v in ln_ps.items()}
 
         else:
-            N_data = next(iter(data.values())).shape[0]
-
             # Compute the bin area for each 2D grid cell for a cheap integral...
             bin_area = {
                 k: np.abs(np.diff(grid1[0])[None] * np.diff(grid2[:, 0])[:, None])
@@ -124,7 +122,7 @@ class ModelMixin:
             }
 
             ln_ns = {
-                k: ln_p + np.log(N_data) + np.log(bin_area[k]) for k, ln_p in ln_ps.items()
+                k: ln_p + np.log(ndata) + np.log(bin_area[k]) for k, ln_p in ln_ps.items()
             }
             ims = {k: np.exp(v) for k, v in ln_ns.items()}
 